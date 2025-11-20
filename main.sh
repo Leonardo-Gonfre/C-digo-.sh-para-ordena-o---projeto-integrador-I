003037cd997c9525e9465a68a36ccfdbfab26703
Eduardo <157843123+theimeduoo@users.noreply.github.com>
2025-11-20 14:53:47 -0300

    Código refatorado funcionando

--- main.sh
+++ main.sh
@@ -1,51 +1,74 @@
-#! /bin/bash
+#!/bin/bash
 
-#Comando esperado: ./main.sh -l <linguagem> -a <algoritmo> -n <execuções> -t <tamanho> -c <caso>
-
-#Obtém os argumentos e armazena-os dentro das variáveis LING, ALG, EXEC, TAM e CASO
-while [[ $1 != "" ]]; do
-    case "$1" in
-        -l) LING=$2;;
-        -a) ALG=$2;;
-        -n) EXEC=$2;;
-        -t) TAM=$2;;
-        -c) CASO=$2;;
-        *) echo "Opções inválidas!"; exit 1;;
-
+# LER PARÂMETROS DO USUÁRIO
+while getopts "l:a:n:t:c:" opt; do
+    case $opt in
+        l) linguagem=$OPTARG ;;   
+        a) algoritmo=$OPTARG ;;   
+        n) repeticoes=$OPTARG ;;  
+        t) tamanho=$OPTARG ;;     
+        c) caso=$OPTARG ;;        
     esac
-    shift
-    shift
-
 done
 
-#Verifica a linguagem definida e escolhe qual arquivo será executado (selectionsort.py, mergesort.py, selectionsort.c ou mergesort.c)
-
-#VERIFICA COM LING = "PYTHON"
-if [[ "$LING" == "python" ]]; then
-    if [[ "$ALG" == "selection" ]]; then
-        FILE="selectionsort.py"
-        CMD="python3 $FILE"
-        
-    elif [[ "$ALG" == "merge" ]]; then
-        FILE="mergesort.py"
-        CMD="python3 $FILE"
-    else
-        echo "Algoritmo inválido! Use: selection ou merge"
-    fi
+# VALIDAR PARÂMETROS
+if [[ -z "$linguagem" || -z "$algoritmo" || -z "$repeticoes" || -z "$tamanho" || -z "$caso" ]]; then
+    echo "ERRO: Parâmetros insuficientes."
+    echo "Uso: ./executaScriptShell -l <linguagem> -a <algoritmo> -n <execuções> -t <tamanho> -c <caso>"
+    exit 1
 fi
 
-#VERIFICA COM LING = "C"
-if [[ "$LING" == "c" ]]; then
-    if [[ "$ALG" == "selection" ]]; then
-        FILE="selectionsort.c"
-        CMD="gcc $FILE -o $ALG | ./$ALG"
-        
-    elif [[ "$ALG" == "merge" ]]; then
-        FILE="mergesort.c"
-        CMD="gcc $FILE -o $ALG | ./$ALG"
+# DEFINIR ARQUIVO DE SAÍDA 
+arquivo_log="log_${linguagem}_${algoritmo}_${tamanho}_${caso}.csv"
+
+echo "tamanho,tempo" > $arquivo_log
+
+# DEFINIR COMANDO DE EXECUÇÃO
+
+if [[ $linguagem == "c" ]]; then
+    
+    if [[ $algoritmo == "merge" ]]; then
+        fonte="merge_c.c"
+        binario="merge_c.out"
+    elif [[ $algoritmo == "selection" ]]; then
+        fonte="selection_c.c"
+        binario="selection_c.out"
     else
-        echo "Algoritmo inválido! Use: selection ou merge"
+        echo "Algoritmo inválido."
+        exit 1
     fi
+
+    echo "Compilando código C..."
+    gcc $fonte -o $binario
+    comando="./$binario $tamanho $caso"
+
+elif [[ $linguagem == "python" ]]; then
+    
+    if [[ $algoritmo == "merge" ]]; then
+        comando="python3 merge_py.py $tamanho $caso"
+    elif [[ $algoritmo == "selection" ]]; then
+        comando="python3 selection_py.py $tamanho $caso"
+    else
+        echo "Algoritmo inválido."
+        exit 1
+    fi
+
+else
+    echo "Linguagem inválida (use c ou python)."
+    exit 1
 fi
 
-$CMD $TAM $CASO
+# EXECUTAR N VEZES
+echo "Executando $repeticoes vezes..."
+
+for ((i=1; i<=repeticoes; i++))
+do
+    linha=$($comando)
+    
+    linha_csv=$(echo "$linha" | sed 's/;/,/')
+    
+    echo "$linha_csv" >> $arquivo_log
+done
+
+echo "Execução concluída!"
+echo "Resultados salvos em: $arquivo_log"
